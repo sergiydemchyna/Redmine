6859f45d39685908f7d599a1d0bf8435c3e1b0f9
Sergiy <sergiydemchyna@gmail.com>
2021-09-13 16:50:12 +0300

    Update redmine.sh

--- redmine.sh
+++ redmine.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+echo 'Acquire::ForceIPv4 "true";' | sudo tee /etc/apt/apt.conf.d/99force-ipv4
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt install build-essential ruby-dev libxslt1-dev libmariadb-dev libxml2-dev zlib1g-dev imagemagick libmagickwand-dev curl gnupg2 bison libbison-dev libgdbm-dev libncurses-dev libncurses5-dev libreadline-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 -y
