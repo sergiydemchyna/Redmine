d3095adac196344824bd8cbffeb2ff26b67e5dda
Sergiy <sergiydemchyna@gmail.com>
2021-09-13 15:15:24 +0300

    Add files via upload

---
+++ network.tf
@@ -0,0 +1,83 @@
+resource "aws_internet_gateway" "prod-igw" {
+    vpc_id = "${aws_vpc.prod-vpc.id}"
+}
+
+resource "aws_route_table" "prod-public-crt" {
+    vpc_id = "${aws_vpc.prod-vpc.id}"
+    
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.prod-igw.id}"
+    }
+}
+
+resource "aws_route_table_association" "prod-crta-public-subnet-1"{
+    subnet_id = "${aws_subnet.prod-subnet-public-1.id}"
+    route_table_id = "${aws_route_table.prod-public-crt.id}"
+}
+
+resource "aws_security_group" "allowed" {
+    vpc_id = "${aws_vpc.prod-vpc.id}"
+
+    ingress {
+        description = "https_to_web"
+        from_port   = 443
+        to_port     = 443
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        description = "http_to_web"
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        description = "https_to_web"
+        from_port   = 3000
+        to_port     = 3000
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        description = "ssh"
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        cidr_blocks = ["0.0.0.0/0"]
+        protocol    = "icmp"
+        from_port   = 8
+        to_port     = 0
+    }
+    ingress {
+        description = "MYSQL"
+        from_port   = 3306
+        to_port     = 3306
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        description = "MYSQL"
+        from_port   = 3307
+        to_port     = 3307
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        protocol  = -1
+        self      = true
+        from_port = 0
+        to_port   = 0
+    }
+
+    egress  {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
